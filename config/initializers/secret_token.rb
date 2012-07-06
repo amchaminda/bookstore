621dc6ca21e37686a0e3e57d798aedb468003899
chams <chams@chams-ThinkPad-T61.(none)>
2012-07-06 10:35:04 +0530

    initila commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '869792eafe79a5f4ad6c25e5882f129a78769946b88df445f3de9c3514aba69b5f77f481e30bdc6a9398923697a7e548d84fbc8c9fa88b6f47d446b7445ee5fa'
